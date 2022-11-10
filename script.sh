6dd6615efd564a77a6af6ba90e17c14f45fa0b08
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-11-10 19:26:55 +0000

    add v1 of the Script

---
+++ script.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo "Defining Vars"
+# define install mode. false = "Just Troop", true = "Troop and foxdot and SuperCollider"
+installMode=$1 || false
+# define current Dir
+currentDir=$pwd
+echo 'Ensuring GH is happy!'
+# Ensure gh CLI is installed
+apt install gh -y
+# Insure logged into gh
+gh auth login
+
+if ($installMode) {
+    # install foxdot
+    echo "Installing FoxDot"
+    cd $currentDir
+    gh repo clone UTCSheffield/FoxDot
+    cd FoxDot
+    python3 -m pip install -e .
+    cd ../..
+    echo "Installed FoxDot"
+    # install SuperCollider
+    echo "Installing SuperCollider"
+    apt install supercollider -y
+    echo "Installed SuperCollider, Please continue quark install and start script setup manually!"
+    # install Troop
+    echo "Installing Troop"
+    cd $currentDir
+    apt install python-tk -y
+    gh repo clone Qirky/Troop
+    echo "Installed Troop"
+} else {
+    # install Troop
+    echo "Installing Troop"
+    cd $currentDir
+    apt install python-tk -y
+    gh repo clone Qirky/Troop
+    echo "Installed Troop"
+}
